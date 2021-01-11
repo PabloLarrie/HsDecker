53bf1d6d74bfe6107eaa359a4831f437f8d3e92d
PabloLarrie <pablo_14_pont@hotmail.com>
2021-01-11 14:14:41 +0100

    adventure updated2

--- adventure/models.py
+++ adventure/models.py
@@ -4,7 +4,7 @@ class Adventure(models.Model): #Primer menú
     announcement = models.CharField(max_length=50, unique=True) #Chapter unlocked! Chapter I:Dalaran Bank
     name = models.CharField(max_length=50, unique=True)  #Dalaran 
     message = models.CharField(max_length=150, unique=True) # Dalaran! A splendid, floating city of...
-    menus = models.ForeignKey('adventure.Chapter', through='adventure.AdventureChapters', on_delete=models.CASCADE) #Selección de capítulo (I, II, III, IV o V)
+    menus = models.ForeignKey('adventure.Chapter', through='adventure.AdventureConf', related_name='adventure', on_delete=models.CASCADE) #Selección de capítulo (I, II, III, IV o V)
 
 
 class Chapter(models.Model): #Segundo menú
@@ -12,23 +12,12 @@ class Chapter(models.Model): #Segundo menú
     name = models.CharField(max_length=50, unique=True) #Dalaran Bank
     message = models.CharField(max_length=150, unique=True) #Everyone, split up. For this plan to work, we must each do our part.
     info = models.CharField(max_length=100, unique=True) #Break into the Bank of Dalaran to loot its most precious artifacts.
-    rewards = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100) #Enseña una imagen del reward.
-    description = models.CharField(max_length=50, unique=True) #First Chapter, bank of Dalaran... (Accesible desde el menú anterior)  
-    adventure = models.ForeignKey('adventure.Adventure', through='adventure.AdventureChapters', on_delete=models.CASCADE)
-    anomaly_get = models.ManyToManyField('adventure.Anomaly', related_name='chapters', on_delete=models.CASCADE) #relación chapter/anomaly
-    new = models.BooleanField() #Indica si se trata de un Challenge nuevo con "New"
+    rewards = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100) #Enseña una imagen del reward. 
+    unlocked = models.BooleanField() #Indica si el Chapter es accesible
+    unlocked_info = models.CharField(max_length=50) #Opens week 2 | Opens week 5
+    menus = models.ManyToManyField('adventure.Champion', through='adventure.AdventureConf', on_delete=models.CASCADE) #relación Chapter/Champion
     #para avanzar al siguiente menú solo hay que darle a "GO!"
     
-
-class AdventureChapter(models.Model): #Tabla de relación
-    adventure = models.ForeignKey('adventure.Adventure', on_delete=models.CASCADE) #Indica la aventura
-    chapter = models.ForeignKey('adventure.Chapter', on_delete=models.CASCADE) #Indica el capítulo
-    info = models.CharField(max_length=50, unique=True) #Dalaran. 0/5 Chapters Completed
-    unlocked = models.BooleanField() #Indica si el chapter es accesible
-    unlocked_info = models.CharField(max_length=50) #Opens week 2 / Still unlocked
-    new = models.BooleanField() #Indica si se trata de un Challenge nuevo con un contorno brillante.
-    reward_status = models.BooleanField() #Indica si se ha conseguido el reward
-    anomaly = models.BooleanField() #Indica si se juega con o sin Anomaly.
 
 class Anomaly(models.Model): #subtrama del segundo menú
     name = models.CharField(max_length=50, unique=True) #Anomaly- Arcane
@@ -36,13 +25,15 @@ class Anomaly(models.Model): #subtrama del segundo menú
     image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100) #imagen de la anomalía
     info = models.CharField(max_length=50, unique=True)  #Anomaly Mode: Gain a random effect this turn.
 
+
 class Champion(models.Model): #Tercer menú
     message = models.CharField(max_length=200, unique=True) #We have recruited an elite band of disenfranchised miscreantes. Each have their own strengths.     
-    unlocked = models.BooleanField() #Indica si el Hero es accesible
-    unlocked_info = models.CharField(max_length=100) #Opens week 2 / Still unlocked
     new = models.BooleanField() #Indica si se trata de un Champion nuevo con "New"
+    unlocked = models.BooleanField() #Indica si el power es accesible
+    unlocked_info = models.CharField(max_length=100) #Opens week 2 | Opens week 5
     hero = models.ManyToManyField('adventure.Hero', related_name='champions', on_delete=models.CASCADE) #Selección del hero.
-    hero = models.ManyToManyField('cards.Hero', related_name='champions', on_delete=models.CASCADE) #Selección del hero.
+    menus = models.ManyToManyField('adventure.Power', through='adventure.AdventureConf', on_delete=models.CASCADE) #relación Champion/Power
+    #hero = models.ManyToManyField('cards.Hero', related_name='champions', on_delete=models.CASCADE) #Selección del hero.
 
 
 """class Hero(models.Model): #heroes
@@ -77,3 +68,17 @@ class Challenge(models.Model): #Sexto menú
     round_ind = models.PositiveIntegerField(default=1) #Indica la ronda en la que se encuentra el jugador.
     hero_enemy = models.ManyToManyField('cards.Hero', related_name='adventure bosses', on_delete=models.CASCADE) #Indicador del Boss.
     bosses = models.PositiveIntegerField(default=0) #indica el número de bosses derrotados    
+
+
+class AdventureConf(models.Model): #Tabla de relación
+    adventure = models.ForeignKey('adventure.Adventure', on_delete=models.CASCADE) #Indica la aventura
+    chapter = models.ForeignKey('adventure.Chapter', on_delete=models.CASCADE) #Indica el capítulo
+    champ = models.ForeignKey('adventure.Champion', on_delete=models.CASCADE) #Indica el hero
+    power = models.ForeignKey('adventure.Power', on_delete=models.CASCADE) #Indica el hero power
+    deck = models.ForeignKey('adventure.Deck', on_delete=models.CASCADE) #Indica el Deck
+    enemy = models.ForeignKey('adventure.Challenge', on_delete=models.CASCADE) #Indica el Enemy
+    info = models.PositiveIntegerField(default=0) #Dalaran. 0/5 Chapters Completed
+    reward_status = models.BooleanField() #Indica si se ha conseguido el reward
+    anomaly = models.BooleanField() #Indica si se juega con o sin Anomaly.
+    anomaly_get = models.ManyToManyField('adventure.Anomaly', related_name='chapters', on_delete=models.CASCADE) #relación chapter/anomaly
+    new = models.BooleanField() #Indica si se trata de un Item nuevo con "New"
