1833152ed582c6474fc61785a753f661f3a027fa
PabloLarrie <pablo_14_pont@hotmail.com>
2021-03-12 12:34:43 +0100

    l+b+d

--- cards/tests/test_views.py
+++ cards/tests/test_views.py
@@ -42,16 +42,17 @@ class TestCardViewSet:
     def test_cards_filter(self):
         collection = CollectionFactory()
         expansion = ExpansionFactory(collection=collection)
+        CardFactory.create_batch(100)
         card = CardFactory(expansion=expansion)
 
         request_factory = APIRequestFactory()
         reverse_url = reverse(
-            "cards:cards-list", kwargs={"SearchFilter": card.name}
-        )  # "http://localhost:8000/cards/cards/X"
-        request = request_factory.get(reverse_url)
-        response = CardViewSet.as_view({"get": "list"})(request, SearchFilter=card.name)
+            "cards:cards-list"
+        )  # "http://localhost:8000/cards/cards/X" (URL correspondiente al listado de cartas)
+        request = request_factory.get(reverse_url, {"search": card.name})
+        response = CardViewSet.as_view({"get": "list"})(request)
+        # devuelve el método que se encarga de devolver la lista de cartas if request
 
         assert response.status_code == 200
-        # assert response.data["id"] == cards[0].id
-        # assert response.data["name"] == cards[0].name
-        # assert response.data["usage"] == cards[0].usage()
+        assert len(response.data) == 1
+        assert response.data[0]["name"] == card.name
