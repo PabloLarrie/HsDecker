03d4f3c1aab1991a6c5fe604faa2965086bbebf2
PabloLarrie <pablo_14_pont@hotmail.com>
2021-04-09 16:49:51 +0200

    updated

---
+++ decks/management/commands/setup_user_permissions.py
@@ -0,0 +1,46 @@
+from django.core.management import BaseCommand
+from django.contrib.auth.models import Group, Permission
+
+
+class Command(BaseCommand):
+    GROUPS = ["Basic", "Admin", "Master"]
+    PERMISSIONS = {
+        "Basic": ["deck", "deckcard"],
+        "Admin": [
+            "deck",
+            "deckcard",
+            "card",
+            "collection",
+            "expansion",
+            "power",
+            "heroclass",
+            "hero",
+            "keyword",
+        ],
+        "Master": [
+            "deck",
+            "deckcard",
+            "card",
+            "collection",
+            "expansion",
+            "power",
+            "heroclass",
+            "hero",
+            "keyword",
+            "user",
+            "permission",
+            "group",
+        ],
+    }
+
+    def handle(self, *args, **kwargs):
+        for v in self.GROUPS:
+            group = Group.objects.create(name=v)
+            for model in self.PERMISSIONS[v]:
+                group.permissions.set([Permission.objects.get(codename="add_" + model)])
+                group.permissions.add(
+                    Permission.objects.get(codename="change_" + model)
+                )
+                group.permissions.add(
+                    Permission.objects.get(codename="delete_" + model)
+                )
